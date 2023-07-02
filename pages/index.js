46745d15c87c85f7df9e8cdc8437e64ad3710a36
Michel Maciel <michel.asm@hotmail.com>
2023-07-02 16:05:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Deschamps, seu curso está massa!</h1>
+  return <h1>Deschamps, seu curso está massa!</h1>;
+}
+
+function testeDoEditorConfig() {
+  console.log("Identação funcionando!");
+}
+
+function testeDoPrettier() {
+  console.log("Testando o Prettier");
 }
 
 export default Home;
